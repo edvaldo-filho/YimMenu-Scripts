6bf81cdec0c78f23793ffe209705c03e46f0a797
Edvaldo Filho <122257316+edvaldo-filho@users.noreply.github.com>
2024-11-29 21:38:12 -0300

    Update Pessi.lua

--- Pessi.lua
+++ Pessi.lua
@@ -10,25 +10,25 @@ function TransactionManager.new()
     local instance = setmetatable({}, TransactionManager);
 
     instance.Transactions = {
-        {label = "15M (Bend Job Limited)", hash = 0x176D9D54},
-        {label = "15M (Bend Bonus Limited)", hash = 0xA174F633},
-        {label = "7M (Gang Money Limited)", hash = 0xED97AFC1},
-        {label = "3.6M (Casino Heist Money Limited)", hash = 0xB703ED29},
-        {label = "2.5M (Gang Money Limited)", hash = 0x46521174},
-        {label = "2.5M (Island Heist Money Limited)", hash = 0xDBF39508},
-        {label = "2M (Heist Awards Money Limited)", hash = 0x8107BB89},
-        {label = "2M (Tuner Robbery Money Limited)", hash = 0x921FCF3C},
-        {label = "2M (Business Hub Money Limited)", hash = 0x4B6A869C},
-        {label = "1M (Avenger Operations Money Limited)", hash = 0xE9BBC247},
-        {label = "1M (Daily Objective Event Money Limited)", hash = 0x314FB8B0},
-        {label = "1M (Daily Objective Money Limited)", hash = 0xBFCBE6B6},
-        {label = "680K (Betting Money Limited)", hash = 0xACA75AAE},
-        {label = "500K (Juggalo Story Money Limited)", hash = 0x05F2B7EE},
-        {label = "310K (Vehicle Export Money Limited)", hash = 0xEE884170},
-        {label = "200K (DoomsDay Finale Bonus Money Limited)", hash = 0xBA16F44B},
-        {label = "200K (Action Figures Money Limited)",  hash = 0x9145F938},
-        {label = "200K (Collectibles Money Limited)",    hash = 0xCDCF2380},
-        {label = "190K (Vehicle Sales Money Limited)",   hash = 0xFD389995}
+        {label = "15M (Bend Job Limited) - JUST 1 CLICK !", hash = 0x176D9D54},
+        {label = "15M (Bend Bonus Limited) - JUST 1 CLICK !", hash = 0xA174F633},
+        {label = "7M (Gang Money Limited) - JUST 1 CLICK !", hash = 0xED97AFC1},
+        {label = "3.6M (Casino Heist Money Limited) - JUST 1 CLICK !", hash = 0xB703ED29},
+        {label = "2.5M (Gang Money Limited) - JUST 1 CLICK !", hash = 0x46521174},
+        {label = "2.5M (Island Heist Money Limited) - JUST 1 CLICK !", hash = 0xDBF39508},
+        {label = "2M (Heist Awards Money Limited) - JUST 1 CLICK !", hash = 0x8107BB89},
+        {label = "2M (Tuner Robbery Money Limited) - JUST 1 CLICK !", hash = 0x921FCF3C},
+        {label = "2M (Business Hub Money Limited) - JUST 1 CLICK !", hash = 0x4B6A869C},
+        {label = "1M (Avenger Operations Money Limited) - JUST 1 CLICK !", hash = 0xE9BBC247},
+        {label = "1M (Daily Objective Event Money Limited) - JUST 1 CLICK !", hash = 0x314FB8B0},
+        {label = "1M (Daily Objective Money Limited) - JUST 1 CLICK !", hash = 0xBFCBE6B6},
+        {label = "680K (Betting Money Limited) - JUST 1 CLICK !", hash = 0xACA75AAE},
+        {label = "500K (Juggalo Story Money Limited) - JUST 1 CLICK !", hash = 0x05F2B7EE},
+        {label = "310K (Vehicle Export Money Limited) - JUST 1 CLICK !", hash = 0xEE884170},
+        {label = "200K (DoomsDay Finale Bonus Money Limited) - JUST 1 CLICK !", hash = 0xBA16F44B},
+        {label = "200K (Action Figures Money Limited) - JUST 1 CLICK !",  hash = 0x9145F938},
+        {label = "200K (Collectibles Money Limited) - JUST 1 CLICK !",    hash = 0xCDCF2380},
+        {label = "190K (Vehicle Sales Money Limited) - JUST 1 CLICK !",   hash = 0xFD389995}
     }
 
     return instance;
@@ -69,7 +69,7 @@ function TransactionManager:TriggerTransaction(item_hash)
 end
 
 function TransactionManager:Init()
-    local tab               = gui.get_tab("Monkey")
+    local tab               = gui.get_tab("Pessi SCRIPT")
     local sub_atm           = tab:add_tab("ATM")
     local sub_transaction   = tab:add_tab("Looped Transactions")
     local checkboxwb        = sub_atm:add_checkbox("Transfer Wallet Money To Bank")
@@ -79,32 +79,12 @@ function TransactionManager:Init()
     local checkbox100k       = sub_transaction:add_checkbox("100K Loop")
     sub_transaction:add_sameline()
     local bountyloop       = sub_transaction:add_checkbox("Bounty Loop")
-    local sub_transactionL  = tab:add_tab("Limited Transactions")
-
-    script.register_looped("1mtransaction", function()
-        if checkbox1m:is_enabled() then
-            self:TriggerTransaction(0x615762F1)
-        end
-    end)
-    
-
-
-    script.register_looped("50ktransaction", function(script)
-        if(checkbox50k:is_enabled()) then
-            self:TriggerTransaction(0x610F9AB4)
-        end
-    end)
-
-    script.register_looped("100ktransaction", function(script)
-        if(checkbox100k:is_enabled()) then 
-            self:TriggerTransaction(0x68341DC5);
-        end
-    end)
+    local sub_transactionL  = tab:add_tab("Limited Transactions - 1 Time click")
 
     script.register_looped("bountyloop", function(script)
         if(bountyloop:is_enabled()) then 
             self:TriggerTransaction(0xC4F96E65);
-            script:sleep(1000);
+            script:sleep(10000);
             self:TriggerTransaction(0x610F9AB4);
         end
     end)
